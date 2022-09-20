ba308bf9522d5eaa52011c41157b34d17e68164e
Muhammed Mahmutoğlu <muhammed.mahmutoglu@outlook.com>
2022-09-21 02:16:09 +0300

    covid-app
    internet üzerinde bulduğum bir APİ kullanarak covid 19 verilerini ülkeler bazında görüntüleyebileceğimiz bir web sitesi haline getirdim.

---
+++ src/components/CountryTable.vue
@@ -0,0 +1,28 @@
+<template>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Date</th>
+                <th scope="col">Confirmed</th>
+                <th scope="col">Deaths</th>
+                <th scope="col">Recovered</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(item, index) in data" :key="index">
+                <th scope="row">{{index + 1}}</th>
+                <td>{{item.Date}}</td>
+                <td>{{item.Confirmed }}</td>
+                <td>{{item.Deaths}}</td>
+                <td>{{item.Recovered }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    props: ['data']
+}
+</script>
